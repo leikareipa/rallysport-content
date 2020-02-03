26bef2ecc5df31d1b3d420b41c57f10ef460fe0d
leikareipa <drilledicecream@gmail.com>
2020-02-03 17:22:37 +0200

    resource-id: Add a list of forbidden resource key words
    Randomly-generated resource keys will now be checked against a list of
    forbidden words (e.g. "fap") and re-generated if a match is found.

--- rallysport-content/common-scripts/resource-id.php
+++ rallysport-content/common-scripts/resource-id.php
@@ -41,19 +41,107 @@ abstract class ResourceType
     public const USER = "user";
 }
 
+// When generating a new resource ID (e.g. "track.xxx-xxx-xxx"), match its key
+// element (e.g. "xxx-xxx-xxx") against this list of forbidden words - if the
+// key contains any of these words, the ID should be discarded and re-generated.
+//
+// Note that although the key element might only contain three characters/letters
+// per segment (e.g. "abc-def-ghi" has the three segments "abc", "def", and
+// "ghi"), words longer than three characters can form when the segments are
+// read together. For this reason, the ::matches() utility function is provided
+// - simply call it with the key you want to test, and if the function returns
+// false, the key does not contain any of the forbidden words.
+//
+abstract class ForbiddenResourceKey
+{
+
+    static public function matches(string $key) : bool
+    {
+        $concatenatedKey = str_replace(ResourceID::RESOURCE_KEY_FRAGMENT_SEPARATOR, "", $key);
+
+        foreach (self::FORBIDDEN_WORDS as $forbiddenWord)
+        {
+            if (preg_match("/{$forbiddenWord}/i", $concatenatedKey))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // The forbidden words, in regex format.
+    public const FORBIDDEN_WORDS = [
+        "f[a4][g9]",
+        "[a4][s5][s5]",
+        "fu[kg9qc]r?",
+        "c[o0u][ckx]",
+        "d[i1][kcx]",
+        "p[e3]n[i1][s5]",
+        "c[l1][t7]",
+        "[g9][a4]y",
+        "[g9][e3a4][i1y]",
+        "ke[ij]",
+        "h[o0]m[o0]",
+        "f[a4]p",
+        "tu[g9]",
+        "j[e3]w",
+        "j[o0][o0]",
+        "juu",
+        "tag",
+        "p[o0][o0]",
+        "[s5]h[i1][t7]",
+        "p[i1][s5][s5]",
+        "nu[t7]",
+        "[g9][e3]n[i1]t[a4][l1]",
+        "[s5][e3]m[e3]n",
+        "[s5][t7]d",
+        "[s5][e3]x",
+        "[s5][e3]k[s5z2]",
+        "[g9][i1][z2][z2]",
+        "j[i1][z2][z2]",
+        "[t7][i1][t7]",
+        "[b6]r[e3][a4][s5][t7]",
+        "n[a4]d",
+        "v[a4]?[g9][i1]n[a4]",
+        "v[g9]n",
+        "v[a4]j",
+        "c[l1][i1][t7]",
+        "c[l1][t7]",
+        "[o0]r[a4][l1]",
+        "[a4]nu[s5]",
+        "[a4]n[a4][l1]",
+        "[a4]n[l1]",
+        "[s5]u[ckg9]",
+        "tup",
+        "[ck]um",
+        "[b68][i1][t7]ch",
+        "wh[o0]r[e3]",
+        "n[i1][g9]",
+        "n[g9]r",
+        "kkk",
+        "c[o0]mm[i1][e3]",
+        "n[a4][z2][i1]",
+        "911",
+        "112",
+        "666",
+        "vv",
+    ];
+}
+
 class ResourceID
 {
     private $resourceIDString;
 
     // The set of characters that the resource key is allowed to use.
-    const RESOURCE_KEY_CHARSET = "23789acefghjkmnprstuv";
+    public const RESOURCE_KEY_CHARSET = "23789acefghjkmnprstuv";
 
     // These two constants should not be changed ever.
-    const RESOURCE_TYPE_SEPARATOR = ".";
-    const RESOURCE_KEY_FRAGMENT_SEPARATOR = "-";
+    public const RESOURCE_TYPE_SEPARATOR = ".";
+    public const RESOURCE_KEY_FRAGMENT_SEPARATOR = "-";
 
-    const RESOURCE_KEY_FRAGMENT_LENGTH = 3;
-    const NUM_RESOURCE_KEY_FRAGMENTS = 3;
+    public const RESOURCE_KEY_FRAGMENT_LENGTH = 3;
+    public const NUM_RESOURCE_KEY_FRAGMENTS = 3;
 
     // Create a resource ID object of the given type and with a random key. On
     // error, returns NULL.
@@ -255,17 +343,30 @@ class ResourceID
             return $randomFragment;
         };
 
-        $randomResourceID = ($resourceType . self::RESOURCE_TYPE_SEPARATOR);
-
-        for ($i = 0; $i < self::NUM_RESOURCE_KEY_FRAGMENTS; $i++)
+        // Generate a random resource key.
+        $numLoops = 0;
+        do
         {
-            $randomResourceID .= $randomKeyFragment();
+            $randomResourceKey = "";
 
-            if ($i < (self::NUM_RESOURCE_KEY_FRAGMENTS - 1))
+            for ($i = 0; $i < self::NUM_RESOURCE_KEY_FRAGMENTS; $i++)
             {
-                $randomResourceID .= self::RESOURCE_KEY_FRAGMENT_SEPARATOR;
+                $randomResourceKey .= $randomKeyFragment();
+    
+                if ($i < (self::NUM_RESOURCE_KEY_FRAGMENTS - 1))
+                {
+                    $randomResourceKey .= self::RESOURCE_KEY_FRAGMENT_SEPARATOR;
+                }
             }
-        }
+
+            if (++$numLoops > 10000)
+            {
+                $randomResourceKey = "invalid";
+                break;
+            }
+        } while (ForbiddenResourceKey::matches($randomResourceKey));
+
+        $randomResourceID = ($resourceType . self::RESOURCE_TYPE_SEPARATOR . $randomResourceKey);
 
         return $randomResourceID;
     }
