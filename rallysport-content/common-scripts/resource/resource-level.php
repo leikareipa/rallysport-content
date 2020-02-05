b1b05290f9a53d1e61776a9531dca44fa83eb376
leikareipa <drilledicecream@gmail.com>
2020-02-05 23:05:47 +0200

    Add the ResourceVisibility enumeration

---
+++ rallysport-content/common-scripts/resource/resource-level.php
@@ -0,0 +1,20 @@
+<?php namespace RSC;
+
+/*
+ * 2020 Tarpeeksi Hyvae Soft
+ * 
+ * Software: Rally-Sport Content (RSC)
+ * 
+ */
+
+// Enumerates the states of public visibility that a resource (e.g. a track or
+// a user) can be assigned.
+abstract class ResourceVisibility
+{
+    // Note: These values may be stored separate from their labels, so their
+    // meaning should never change.
+    public const NONE     = 0; // Nobody can publically access this resource (e.g. because it has been deleted).
+    public const UNLISTED = 1; // The resource will not be shown in listing of its resource type, but can be accessed publically through its resource ID.
+    public const PRIVATE  = 2; // The resource can be accessed publically only by the user account that uploaded this resource.
+    public const EVERYONE = 3; // The visibility of this resource is not limited.
+}
