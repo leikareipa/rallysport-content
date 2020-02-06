3ffdd797d2a6b9f92392cc7d2fbbe2587e3233e1
leikareipa <drilledicecream@gmail.com>
2020-02-07 00:42:33 +0200

    html-fragment: Add basic header and footer fragments

---
+++ rallysport-content/common-scripts/html-page/html-page-fragments/rallysport-content-footer.php
@@ -0,0 +1,31 @@
+<?php namespace RSC\HTMLPage\Fragment;
+
+/*
+ * 2020 Tarpeeksi Hyvae Soft
+ * 
+ * Software: Rally-Sport Content
+ * 
+ */
+
+require_once __DIR__."/html-page-fragment.php";
+require_once __DIR__."/../../resource/resource-visibility.php";
+
+// A basic footer element intended to be displayed on Rally-Sport Content's
+// HTML pages.
+class RallySportContentFooter extends HTMLPageFragment
+{
+    static public function css() : string
+    {
+        return file_get_contents(__DIR__."/css/rallysport-content-footer.css");
+    }
+
+    static public function html()
+    {
+        return "
+        <footer id='rallysport-content-footer'>
+            Rally-Sport Content &copy; 2020 Tarpeeksi Hyvae Soft.
+            <a href='https://www.github.com/leikareipa/rallysport-content/'>See on GitHub.</a>
+        </footer>
+        ";
+    }
+}
---
+++ rallysport-content/common-scripts/html-page/html-page-fragments/rallysport-content-header.php
@@ -0,0 +1,30 @@
+<?php namespace RSC\HTMLPage\Fragment;
+
+/*
+ * 2020 Tarpeeksi Hyvae Soft
+ * 
+ * Software: Rally-Sport Content
+ * 
+ */
+
+require_once __DIR__."/html-page-fragment.php";
+require_once __DIR__."/../../resource/resource-visibility.php";
+
+// A basic header element intended to be displayed on Rally-Sport Content's
+// HTML pages.
+class RallySportContentHeader extends HTMLPageFragment
+{
+    static public function css() : string
+    {
+        return file_get_contents(__DIR__."/css/rallysport-content-header.css");
+    }
+
+    static public function html()
+    {
+        return "
+        <header id='rallysport-content-header'>
+            Rally-Sport Content
+        </header>
+        ";
+    }
+}
