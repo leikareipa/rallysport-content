f44ad3af5ef849869f81b60318a91c132895893b
leikareipa <drilledicecream@gmail.com>
2020-02-10 04:24:45 +0200

    html page: Move header's login element into its own...
    ...component class.

---
+++ rallysport-content/common-scripts/html-page/html-page-components/login-widget.php
@@ -0,0 +1,49 @@
+<?php namespace RSC\HTMLPage\Fragment;
+      use RSC\HTMLPage;
+
+/*
+ * 2020 Tarpeeksi Hyvae Soft
+ * 
+ * Software: Rally-Sport Content
+ * 
+ */
+
+require_once __DIR__."/../html-page-component.php";
+require_once __DIR__."/../../resource/resource-visibility.php";
+require_once __DIR__."/../../resource/resource-id.php";
+
+// Displays a widget with two possible states:
+//
+//   State 1: If the user is currently logged in, the widget displays the
+//            logged-in username, and a link with which the user can log out.
+//
+//   State 2: If the user is not currently logged in, the widget displays
+//            links with which the user can log in or register a new account.
+//
+abstract class LoginWidget extends HTMLPage\HTMLPageComponent
+{
+    static public function css() : string
+    {
+        return file_get_contents(__DIR__."/css/login-widget.css");
+    }
+
+    static public function html()
+    {
+        return "
+        <div class='login-widget'>
+
+            ".(isset($_SESSION["user_resource_id"])
+            ?
+            "<span><i class='fas fa-fw fa-sm fa-user'></i><b>{$_SESSION["user_resource_id"]}</b></span>
+             <span class='separator'>|</span>
+             <a href='/rallysport-content/logout.php'>Log out</a>"
+            :
+            "<a href='/rallysport-content/?form=login'>Log in</a>
+             <span class='separator'>|</span>
+             <a href='/rallysport-content/users/?form=add'>Register</a>"
+            )."
+
+        </div>
+        ";
+    }
+}
--- rallysport-content/common-scripts/html-page/html-page-components/rallysport-content-header.php
+++ rallysport-content/common-scripts/html-page/html-page-components/rallysport-content-header.php
@@ -10,6 +10,7 @@
 
 require_once __DIR__."/../html-page-component.php";
 require_once __DIR__."/../../resource/resource-visibility.php";
+require_once __DIR__."/login-widget.php";
 require_once __DIR__."/../../resource/resource-id.php";
 
 // A basic header element intended to be displayed on Rally-Sport Content's
@@ -18,7 +19,8 @@ abstract class RallySportContentHeader extends HTMLPage\HTMLPageComponent
 {
     static public function css() : string
     {
-        return file_get_contents(__DIR__."/css/rallysport-content-header.css");
+        return file_get_contents(__DIR__."/css/rallysport-content-header.css")
+               . LoginWidget::css();
     }
 
     static public function html()
@@ -28,20 +30,7 @@ abstract class RallySportContentHeader extends HTMLPage\HTMLPageComponent
 
             Rally-Sport Content
 
-            ".(isset($_SESSION["user_resource_id"])
-            ?
-            "<div id='header-login-container'>
-                <span><i class='fas fa-fw fa-sm fa-user'></i><b>{$_SESSION["user_resource_id"]}</b></span>
-                <span class='separator'>|</span>
-                <a href='/rallysport-content/logout.php'>Log out</a>
-            </div>"
-            :
-            "<div id='header-login-container'>
-                <a href='/rallysport-content/?form=login'>Log in</a>
-                <span class='separator'>|</span>
-                <a href='/rallysport-content/users/?form=add'>Register</a>
-            </div>"
-            )."
+            ".LoginWidget::html()."
 
         </header>
         ";
