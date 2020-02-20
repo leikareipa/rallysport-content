7e15623c64766f6f07d0d3d8d14a2c19ec6705b2
leikareipa <drilledicecream@gmail.com>
2020-02-20 16:52:27 +0200

    html-view/navibar: Indicate which page we're currently on
    The button on the navibar corresponding to the current resource URL
    is now drawn in white, to provide a visual indication of the user's
    current location.
    
    Note that this is a preliminary implementaion and will likely be tweaked
    or re-implemented in the future.

--- rallysport-content/common-scripts/html-page/html-page-components/rallysport-content-navibar.php
+++ rallysport-content/common-scripts/html-page/html-page-components/rallysport-content-navibar.php
@@ -11,8 +11,9 @@
 require_once __DIR__."/../html-page-component.php";
 require_once __DIR__."/../../resource/resource-visibility.php";
 
-// A basic footer element intended to be displayed on Rally-Sport Content's
-// HTML pages.
+// A navigation bar intended to be displayed on each Rally-Sport Content page.
+// It provides the user with clickable buttons to navigate to HTML views of the
+// various resources hosted on RSC, like tracks and users.
 abstract class RallySportContentNavibar extends HTMLPage\HTMLPageComponent
 {
     static public function css() : string
@@ -22,17 +23,35 @@ abstract class RallySportContentNavibar extends HTMLPage\HTMLPageComponent
 
     static public function html()
     {
+        // Figure out which page we're on.
+        /// TODO: Don't hard-code URLs.
+        if (strpos($_SERVER["REQUEST_URI"], "/rallysport-content/tracks") !== FALSE)
+        {
+            $currentPage = "tracks";
+        }
+        else if (strpos($_SERVER["REQUEST_URI"], "/rallysport-content/users") !== FALSE)
+        {
+            $currentPage = "users";
+        }
+        else
+        {
+            $currentPage = "home";
+        }
+
         return "
         <div id='rallysport-content-navibar'>
-            <a href='/rallysport-content/' title='Home'>
+            <a href='/rallysport-content/' title='Home'
+               class='button ".(($currentPage == "home")? "current-page" : "")."'>
                 <i class='fas fa-fw fa-home'></i>
             </a>
 
-            <a href='/rallysport-content/tracks/' title='Tracks'>
+            <a href='/rallysport-content/tracks/' title='Tracks'
+               class='button ".(($currentPage == "tracks")? "current-page" : "")."'>
                 <i class='fas fa-fw fa-road'></i>
             </a>
 
-            <a href='/rallysport-content/users/' title='Users'>
+            <a href='/rallysport-content/users/' title='Users'
+               class='button ".(($currentPage == "users")? "current-page" : "")."'>
                 <i class='fas fa-fw fa-users'></i>
             </a>
         </div>
